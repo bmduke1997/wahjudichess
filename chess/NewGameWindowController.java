5ed6062b64e48f5ebcf17fae0f6a9f6bcc7ff4c3
unternehmen <unternehmen@users.noreply.github.com>
2016-11-02 18:07:47 -0400

    Added more to New Game window

--- chess/NewGameWindowController.java
+++ chess/NewGameWindowController.java
@@ -7,6 +7,7 @@ import javafx.beans.binding.BooleanBinding;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleGroup;
@@ -17,6 +18,14 @@ import javafx.stage.Stage;
 
 public class NewGameWindowController implements Initializable {
     private BooleanBinding whiteGoesFirst;
+    private BooleanBinding blackIsHuman;
+    private BooleanBinding whiteIsHuman;
+
+    @FXML
+    private ComboBox<String> blackTypeComboBox;
+
+    @FXML
+    private ComboBox<String> whiteTypeComboBox;
 
     @FXML
     private Button cancelButton;
@@ -31,6 +40,8 @@ public class NewGameWindowController implements Initializable {
     private void playButtonAction() {
         System.out.println((whiteGoesFirst.get() ? "White" : "Black")
                            + " goes first.");
+        System.out.println("Black will be a " + (blackIsHuman.get() ? "human" : "robot") + ".");
+        System.out.println("White will be a " + (whiteIsHuman.get() ? "human" : "robot") + ".");
         ((Stage)playButton.getScene().getWindow()).close();
     }
 
@@ -53,6 +64,44 @@ public class NewGameWindowController implements Initializable {
 
                 value = (String)whoGoesFirst.getSelectedToggle().getUserData();
                 if (value.equals("White")) {
+			return true;
+		} else {
+			return false;
+		}
+            }
+        };
+
+        /* Bond that turns the type of player of black into a bool. */
+        blackIsHuman = new BooleanBinding() {
+            {
+                super.bind(blackTypeComboBox.valueProperty());
+            }
+
+            @Override
+            protected boolean computeValue() {
+                String value;
+
+                value = (String)blackTypeComboBox.getValue();
+                if (value.equals("Human")) {
+			return true;
+		} else {
+			return false;
+		}
+            }
+        };
+
+        /* Bond that turns the type of player of white into a bool. */
+        whiteIsHuman = new BooleanBinding() {
+            {
+                super.bind(whiteTypeComboBox.valueProperty());
+            }
+
+            @Override
+            protected boolean computeValue() {
+                String value;
+
+                value = (String)whiteTypeComboBox.getValue();
+                if (value.equals("Human")) {
 			return true;
 		} else {
 			return false;
