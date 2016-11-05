72d53c96ab6d130ce50c63977fa93e1b4c2e4ec0
unternehmen <unternehmen@users.noreply.github.com>
2016-11-05 18:02:54 -0400

    Moved new Board into the package structure

--- chess/board/Board.java
+++ chess/board/Board.java
@@ -16,4 +16,44 @@ public class Board {
     public Piece[][] getPlayingBoard(){
         return playingBoard;
     }
+
+    public boolean isLegalMove(Movement[] myMovements, int targetX, int targetY)
+    {
+        boolean found = false;
+        for (int i = 0; i < myMovements.length; i++)
+        {
+            if (myMovements[i].getX() == targetX && myMovements[i].getY() == targetY)
+            {
+                found = true;
+            }
+        }
+        if (found == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean isCapture(Piece piece, Movement[] myMovements, int targetX, int targetY)
+    {
+        boolean capture = false;
+        for (int i = 0; i < myMovements.length; i++)
+        {
+            if (myMovements[i].getX() == targetX && myMovements[i].getY() == targetY && playingBoard[targetX][targetY] != null && playingBoard[targetX][targetY].getColor() != piece.getColor())
+            {
+                capture = true;
+            }
+        }
+        if (capture == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
