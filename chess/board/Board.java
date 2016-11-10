2489439f9f5217332cad654326610b0d47373763
SuckBoyTheMagnificent <speedmetalextremist@gmail.com>
2016-11-09 21:02:47 -0500

    Added Compulsive Capture
    Added compulsive capture under board for pieces. 
    
    Only works for individual pieces, isn't a team wide restriction on movement.

--- chess/board/Board.java
+++ chess/board/Board.java
@@ -25,6 +25,7 @@ public class Board {
         }
     }
 
+    /* Make move */
     public void move(int srcX, int srcY, int destX, int destY) {
         System.out.println("Executing move...");
 
@@ -42,10 +43,25 @@ public class Board {
         playingBoard[srcY][srcX] = null;
     }
 
-    public void remove(int x, int y) {
-        if (playingBoard[y][x] == null)
-            return;
+    /* Make move when restricted */
+    public void restrictedMove(int srcX, int srcY, int destX, int destY)
+    {
+        Piece piece = playingBoard[srcY][srcX];
 
+        if(playingBoard[destY][destX] != null) {
+            if (playingBoard[destY][destX].getColor() != piece.getColor()) {
+                System.out.println("Executing restricted move...");
+                playingBoard[destY][destX] = piece;
+                piece.setPosition(destX, destY);
+                playingBoard[srcY][srcX] = null;
+            }
+        }
+    }
+
+    public void remove(int x, int y) {
+        if (playingBoard[y][x] == null) {
+            return;
+        }
         playingBoard[y][x].setPosition(-1, -1);
 
         playingBoard[y][x] = null;
@@ -55,50 +71,58 @@ public class Board {
         return playingBoard[y][x];
     }
 
-    public boolean isLegalMove(Board board, Piece piece, Movement[] myMovements, int targetX, int targetY)
-    {
-        boolean found = false;
+    /* Check if desired move is legal */
+    public boolean hasLegalMove;
+    public void isLegalMove(Board board, Piece piece, Movement[] myMovements, int targetX, int targetY) {
+
+        hasLegalMove = false;
 
         for (int i = 0; i < myMovements.length; i++) {
             if (myMovements[i] == null) break;
 
             else if ( board.getPieceAt(targetX, targetY) == null
                     && myMovements[i].getX() == targetX && myMovements[i].getY() == targetY) {
-                found = true;
+                    hasLegalMove = true;
             }
             else if (myMovements[i].getX() == targetX && myMovements[i].getY() == targetY
                     && board.getPieceAt(targetX, targetY).getColor() != piece.getColor()) {
-                found = true;
+                hasLegalMove = true;
             }
-        }
-
-        if (found == true)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
         }
     }
 
-    public boolean isCapture(Piece piece, Movement[] myMovements, int targetX, int targetY)
-    {
-        boolean capture = false;
-        for (int i = 0; i < myMovements.length; i++)
-        {
-            if (myMovements[i].getX() == targetX && myMovements[i].getY() == targetY && playingBoard[targetY][targetX] != null && playingBoard[targetY][targetX].getColor() != piece.getColor())
-            {
-                capture = true;
+    /* Check for captures for selected piece */
+    public boolean restricted;
+    public void checkRestrictions(Board board, Piece piece, Movement[] myMovements, int targetX, int targetY) {
+
+        restricted = false;
+
+        for (int i = 0; i < myMovements.length; i++) {
+            if (myMovements[i] != null) {
+                if(board.getPieceAt(myMovements[i].getX(), myMovements[i].getY()) == null) {
+                    continue;
+                }
+                else if (board.getPieceAt(myMovements[i].getX(), myMovements[i].getY()).getColor() != piece.getColor()) {
+                    restricted = true;
+                    System.out.println("Is restricted!");
+                    break;
+                }
             }
         }
-        if (capture == true)
-        {
-            return true;
+        if(restricted){
+            int i = 0;
+            int j = 0;
+            while(i < myMovements.length) {
+                if (myMovements[i] == null){
+                    i++;
+                }
+                else {
+                    myMovements[j] = myMovements[i];
+                    j++;
+                    i++;
+                }
+            }
         }
-        else
-        {
-            return false;
-        }
+        isLegalMove(board, piece, myMovements, targetX, targetY);
     }
 }
